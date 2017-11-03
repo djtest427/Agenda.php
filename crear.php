fbc1b508de9f955366f2979b7df9e1d9b340439c
Northerchild <osarus-13@hotmail.com>
2017-11-03 16:56:33 -0500

    El formulario esta listo y los contactos creado ya se pueden ver en la base de datos

--- crear.php
+++ crear.php
@@ -9,6 +9,9 @@
 try
   {
     require_once("funciones/bd_conexion.php");
+    $sql = "INSERT INTO `contactos` (`id`, `nombre`, `telefono`) ";
+    $sql .= "VALUES (NULL, '{$nombre}','{$numero}');";
+    $resultado = $conn->query($sql);
   }
   catch(Exception $e)
     {
@@ -30,6 +33,18 @@ try
   <body>
     <div class="contenedor">
       <h1>Agenda de Contactos</h1>
-
+       <div class="contenido">
+              <?php
+           if($resultado){
+             echo "Contacto Creado";
+           }
+           else
+           {
+             echo "Error" . $conn->error;
+           }
+         ?>
+         <a class="volver" href="index.php">Volver Inicio</a>
+       </div>
+    </div>
   </body>
 </html>
